39b922f844bfff4d1b982e0e2b81f5a8f7b2e5b9
woury94 <146062130+woury94@users.noreply.github.com>
2023-11-19 23:34:14 +0100

    Delete exercice 6 directory

--- exercice 6/main.js
+++
@@ -1,9 +0,0 @@
-let mycharacter = {
-    firstname : "Zoro",
-    lastname : "Roronoa", 
-    age : 21
-}
-
-let sentence = " Mon personnage a " + mycharacter.age + " en age ? "
-
-console.log(sentence)
