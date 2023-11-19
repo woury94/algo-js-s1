74841148d1a7ecc6796f93d554733724324a7e8e
woury94 <woury5855@gmail.com>
2023-11-19 21:45:06 +0100

    new

---
+++ rendu coding/js/script.js
@@ -0,0 +1,7 @@
+/*Toggle Menu*/
+function toggleMenu() {
+	var menuToggle = document.querySelector('.toggle');
+	var navigation = document.querySelector('.navigation');
+	menuToggle.classList.toggle('active');
+	navigation.classList.toggle('active');
+}
